2eb61b1048b005cd60d18fd7e7ec18c3bad15d4e
Debashish-hub <debashish1900@gmail.com>
2022-02-19 10:03:48 +0530

    Added LocationFound

--- Example/LocationFound/ViewController.swift
+++ Example/LocationFound/ViewController.swift
@@ -7,9 +7,14 @@
 //
 
 import UIKit
+import LocationFound
+
+
 
 class ViewController: UIViewController {
 
+    let lUtility = LocationFound.LocationManager()
+    @IBOutlet weak var locationLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,5 +25,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func getLocClicked(_ sender: Any) {
+        if lUtility.startTracking() {
+            print("Tracking Started")
+            update()
+        }
+    }
+    func update() {
+        lUtility.getCurrentAddress { (addr) in
+            print("Current Address: \(addr)")
+            self.locationLabel.text = addr
+        }
+    }
 }
 
